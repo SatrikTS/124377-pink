03fb2b81c129598aa9313686f6baab7f6b9826ff
SatrikTS <satrik90@gmail.com>
2016-12-11 15:58:37 +0300

    5 модуль. Первая страница. моб.версия - 100% и планшетная 100%. десктопная 100%. Страница фото - моб.версия -100%

--- js/app.js
+++ js/app.js
@@ -2,5 +2,21 @@ var switchMenu = document.querySelector(".main-nav__toogle");
 var openMenu = document.querySelector(".main-nav__wrapper");
 
 switchMenu.addEventListener("click", function (event) {
+  event.preventDefault();
   openMenu.style.display = (openMenu.style.display == 'none') ? 'block' : 'none'
+});
+
+var sliderContainer = document.querySelector(".reviews__items");
+var leftControl = document.querySelector(".reviews__controller--left");
+var rightControl = document.querySelector(".reviews__controller--right");
+var step = 100;
+
+leftControl.addEventListener("click", function(event){
+  event.preventDefault();
+  sliderContainer.style.left= -step + "%";
+});
+
+rightControl.addEventListener("click", function(event){
+  event.preventDefault();
+  sliderContainer.style.left= step + "%";
 });
