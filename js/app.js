b19e827b63723a96c61b3bc356998f16acdc99c1
SatrikTS <satrik90@gmail.com>
2016-12-21 00:45:10 +0300

    3 страницы. остались мелкие детали

--- js/app.js
+++ js/app.js
@@ -3,29 +3,6 @@ var openMenu = document.querySelector(".main-nav__wrapper");
 
 switchMenu.addEventListener("click", function (event) {
   event.preventDefault();
-  openMenu.style.display = (openMenu.style.display == 'none') ? 'block' : 'none'
-});
-
-var sliderElement = document.querySelectorAll(".reviews__item");
-var leftControl = document.querySelector(".reviews__controller--left");
-var rightControl = document.querySelector(".reviews__controller--right");
-
-
-leftControl.addEventListener("click", function(event){
-  event.preventDefault();
-  for (var i=0; i<sliderElement.length; i++){
-
-      sliderElement[i].classList.remove("reviews__item--show");
-      sliderElement[i=i+1].classList.add("reviews__item--show");
-}
-
-});
-
-rightControl.addEventListener("click", function(event){
-  event.preventDefault();
-  for (var i=0; i<sliderElement.length; i++){
-      sliderElement[i].classList.remove("reviews__item--show");
-      sliderElement[i=i-1].classList.add("reviews__item--show");
-}
-
+  openMenu.style.display = (openMenu.style.display == 'none') ? 'block' : 'none';
+  switchMenu.classList.add("main-nav__toogle--hide");
 });
