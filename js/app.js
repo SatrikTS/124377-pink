abbdcd9300bfee682614373ea02c15f7713d2a91
SatrikTS <satrik90@gmail.com>
2016-12-22 09:53:36 +0300

    Update app.js
    Правка JS

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ var switchMenu = document.querySelector(".main-nav__toogle");
 var openMenu = document.querySelector(".main-nav__wrapper");
 
 switchMenu.addEventListener("click", function (event) {
-  event.preventDefault();
   openMenu.style.display = (openMenu.style.display == 'none') ? 'block' : 'none';
-  switchMenu.classList.add("main-nav__toogle--hide");
+  switchMenu.classList.toggle("main-nav__toogle--hide");
 });
