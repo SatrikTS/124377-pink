50f916440d504765f269e23064474d07488b5880
SatrikTS <satrik90@gmail.com>
2016-12-24 02:57:23 +0300

    На проверку по базовым критериям. Плюс автоматизация - не доделанная

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -20,6 +22,28 @@ gulp.task("style", function() {
     .pipe(server.stream());
 });
 
+/*ТУТ НАЧАЛО*/
+gulp.task("fonts", function () {
+  gulp.src("fonts/*.{woof, woof2}")
+  .pipe(plumber())
+  .pipe(gulp.dest("fonts"))
+})
+
+gulp.task("images", function () {
+  gulp.src("img/*.{png,jpg}")
+  pipe(imagemin[
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ])
+  .pipe(gulp.dest("img"));
+})
+
+gulp.task("html", function () {
+  gulp.src("*/*.html")
+  .pipe(plumber())
+  .pipe(gulp.dest("*/"))
+})
+
 gulp.task("serve", ["style"], function() {
   server.init({
     server: ".",
@@ -28,6 +52,22 @@ gulp.task("serve", ["style"], function() {
     cors: true,
     ui: false
   });
+
+gulp.task("build", function(fn){
+  run("style", "fonts", "images", "html", fn)
+})
+
+gulp.task("copy", function(){
+  return gulp.src([
+    "style/**/*.{woof,woof2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ switchMenu.addEventListener("click", function (event) {
   openMenu.style.display = (openMenu.style.display == 'none') ? 'block' : 'none';
   switchMenu.classList.toggle("main-nav__toogle--hide");
 });
+
+var showPopup = document.querySelector(".form__button");
+var errorPopup = document.querySelector(".popup--error");
+var successPopup = document.querySelector(".popup--success");
