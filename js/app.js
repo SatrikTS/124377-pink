6d1c2e2c166196bd2c3968254dce60c466c5e520
SatrikTS <satrik90@gmail.com>
2016-12-24 16:48:46 +0300

    Предздача. Правки по макету. без галп

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ switchMenu.addEventListener("click", function (event) {
   switchMenu.classList.toggle("main-nav__toogle--hide");
 });
 
+var filtersIco = document.querySelectorAll(".filters__ico");
+var filtersBtn = document.querySelectorAll(".filters__btn");
+
+for (var i=0; i<filtersBtn.length; i++){
+  filtersBtn[i].addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(121);
+    for (var j=0; j<filtersIco.length; j++){
+      filtersIco[j].classList.toggle ("filters__ico--active");
+    }
+  })
+}
+
 var showPopup = document.querySelector(".form__button");
 var errorPopup = document.querySelector(".popup--error");
 var successPopup = document.querySelector(".popup--success");
