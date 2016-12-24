ff2f49f0e744cb2b8e2dfbca25ba82b879efef68
SatrikTS <satrik90@gmail.com>
2016-12-24 21:44:40 +0300

    Предварительная отправка проекта на проверку. По базовымкритериям

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,16 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
+
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -20,6 +29,43 @@ gulp.task("style", function() {
     .pipe(server.stream());
 });
 
+gulp.task("style-build", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 2 versions"
+      ]}),
+      mqpacker({
+        sort: true
+      })
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("symbols", function(){
+  return gulp.src("img/*.svg")
+  .pipe(svgmin())
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("symbols.svg"))
+  .pipe(gulp.dest("img"));
+});
+
+gulp.task("images", function () {
+  gulp.src("img/*.{png,jpg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
+
 gulp.task("serve", ["style"], function() {
   server.init({
     server: ".",
@@ -32,3 +78,25 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
+
+gulp.task("clean", function(){
+  return del("build");
+})
+
+gulp.task("copy", function(){
+  return gulp.src([
+    "less/*",
+    "fonts/*",
+    "img/*",
+    "js/*",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+
+gulp.task("build", function(fn){
+  run("clean", "copy", "style-build", "images", "symbols", fn)
+})
--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Меню
 var switchMenu = document.querySelector(".main-nav__toogle");
 var openMenu = document.querySelector(".main-nav__wrapper");
 
@@ -6,15 +7,21 @@ switchMenu.addEventListener("click", function (event) {
   switchMenu.classList.toggle("main-nav__toogle--hide");
 });
 
+//Фото-фильтр
 var filtersIco = document.querySelectorAll(".filters__ico");
 var filtersBtn = document.querySelectorAll(".filters__btn");
+var filtersRange = document.querySelectorAll(".range__control")
 
 for (var i=0; i<filtersBtn.length; i++){
   filtersBtn[i].addEventListener("click", function(event){
     event.preventDefault();
-    console.log(121);
+
     for (var j=0; j<filtersIco.length; j++){
       filtersIco[j].classList.toggle ("filters__ico--active");
+    }
+
+    for (var k=0; k<filtersRange.length; k++){
+      filtersRange[k].classList.toggle("range__control--active");
     }
   })
 }
